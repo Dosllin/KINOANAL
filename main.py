25cc4fb97062b4a6490cbf73c80429124b297d1f
Никита Прохоров <89354375+Dosllin@users.noreply.github.com>
2025-11-14 01:43:05 +0300

    Update main.py
    Добавленны коментарии для более удобного редактирования кода для других пользователей

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 from random import randint
+
+
 # Определение классов Film и User
 class Film:
     def __init__(self, id_film, title, genre, director, year, rating):
-        self.id_film = id_film
-        self.title = title
-        self.genre = genre
-        self.director = director
-        self.year = year
-        self.rating = rating
-        self.__observers = []
+        self.id_film = id_film # уникальный идентификатор фильма
+        self.title = title # название фильма
+        self.genre = genre # жанр фильма
+        self.director = director # режиссер фильма
+        self.year = year # год выпуска фильма
+        self.rating = rating # список оценок фильма
 
     def __str__(self):
-        return f"Film(ID: {self.id_film}, Title: {self.title}, Genre: {self.genre}, Director: {self.director}, Year: {self.year}, Rating: {self.rating})"
+        return f"Film(ID: {self.id_film}, Title: {self.title}, Genre: {self.genre}, Director: {self.director}, Year: {self.year}, Rating: {self.rating})" # строковое представление фильма для удобства вывода
 
 
 class User:
     def __init__(self, id_user, name, user_viewed_films, user_genre):
-        self.id_user = id_user
-        self.name = name
-        self.user_viewed_films = user_viewed_films
-        self.user_genre = user_genre
+        self.id_user = id_user # уникальный идентификатор пользователя
+        self.name = name # имя пользователя
+        self.user_viewed_films = user_viewed_films # список просмотренных пользователем фильмов
+        self.user_genre = user_genre # список предпочитаемых жанров пользователя
 
     def __str__(self):
-        return f"User(ID: {self.id_user}, Name: {self.name}, Viewed Films: {[film.title for film in self.user_viewed_films]}, Preferred Genre: {self.user_genre})"
+        return f"User(ID: {self.id_user}, Name: {self.name}, Viewed Films: {[film.title for film in self.user_viewed_films]}, Preferred Genre: {self.user_genre})" # строковое представление пользователя для удобства вывода
+
 
 # Список всех жанров фильмов
 list_all_genre = ["action", "adventure", "animation", "biography", "comedy", "crime", "documentary", "drama", "fantasy",
                   "historical", "horror", "musical", "mystery", "romance", "science fiction", "thriller",
                   "war", "western", "family", "film noir", "coming-of-age", "superhero", "psychological", "satire"]
+
+
 # Менеджер для работы с фильмами и пользователями
 class FilmManager():
-    def __init__(self,user):
+    def __init__(self, user):
         self.user = user
 
     def add_film(self, film):
-        self.user.user_viewed_films.append(film)
+        self.user.user_viewed_films.append(film)  # Добавление фильма в просмотренные пользователем
 
     @staticmethod
     def add_user_review(film, review):
-        film.rating.append(review)
+        film.rating.append(review)  # Добавление оценки к фильму
 
 
 users = {
 
-}#Сюда добавлять пользователей при регистрации
-
-
+}  # Сюда добавлять пользователей при регистрации
 
 # #### Тестовые данные ####
 # film1 = Film(1, "Inception", "science fiction", "Christopher Nolan", 2010, [9, 10, 8])
@@ -61,14 +63,14 @@ users = {
 # manager1.add_film(film3)
 # manager1.add_user_review(film1, 10)
 # print(user1)
-last_id = 0 # переменная для отслеживания последнего ID пользователя, чтобы при регистрации создавать уникальные ID
+last_id = 0  # переменная для отслеживания последнего ID пользователя, чтобы при регистрации создавать уникальные ID
 while True:
     print('1. Войти',
           '2. Зарегистрироваться',
           '3. Выйти из программы', sep='\n')
 
     choice = input('Выберите действие: ')
-    if choice == '1':
+    if choice == '1':  # Вход
         name = input('Введите имя: ')
         if name in users.keys():
             print('Добро пожаловать обратно,', name)
@@ -79,12 +81,13 @@ while True:
     elif choice == '2':
         name = input('Введите имя: ')
         print('Доступные жанры:', ', '.join(list_all_genre))
-        preferred_genre = input('Введите предпочитаемые жанры: ').replace(' ','').lower()
-        new_user = User(last_id+1, name, [], preferred_genre.split(','))
-        print(new_user)
+        preferred_genre = input('Введите предпочитаемые жанры: ').replace(' ',
+                                                                          '').lower()  # Убираем пробелы и приводим к нижнему регистру
+        new_user = User(last_id + 1, name, [], preferred_genre.split(','))  # Создаем нового пользователя
+        last_id += 1  # Обновляем последний ID
         users[new_user.name] = new_user
-
-
+        print('Регистрация успешна.')
+        print(new_user)
     elif choice == '3':
         print('Выход из программы')
         break
