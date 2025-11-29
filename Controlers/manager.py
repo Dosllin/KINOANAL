bbdfd1fdd3a6f5cb780d94b73b88438259f6e382
ilya0735 <iliaros0707@gmail.com>
2025-11-29 03:17:40 +0300

    Update manager.py

--- Controlers/manager.py
+++ Controlers/manager.py
@@ -1,1 +1,14 @@
+# Менеджер для работы с фильмами и пользователями
+class FilmManager:
+    def __init__(self, user):
+        self.user = user
 
+    def add_film_in_viewed(self, film):
+        self.user.user_viewed_films.append(film)  # Добавление фильма в просмотренные пользователем
+
+    def add_film_in_wish_list(self, film):
+        self.user.user_wish_list.append(film)
+
+    @staticmethod
+    def add_user_review(film, review):
+        film.rating.append(review)  # Добавление оценки к фильму
