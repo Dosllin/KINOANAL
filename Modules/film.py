56c37523716d600a4ff900345c6f4e3bae32be4d
ilya0735 <iliaros0707@gmail.com>
2025-11-29 03:18:38 +0300

    Update film.py

--- Modules/film.py
+++ Modules/film.py
@@ -1,1 +1,14 @@
+from dataclasses import dataclass, field
+from typing import List
 
+# Определение класса Film
+@dataclass(kw_only=True)
+class Film:
+    id_film: int
+    title: str
+    genre: List[str] = field(default_factory=list)
+    director: str
+    year: int
+    rating: List[int] = field(default_factory=list)
+    description: str
+    image: str
