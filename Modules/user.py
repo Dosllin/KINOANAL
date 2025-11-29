4509f7dcc8b2214dbbf3edadc761a786126a9f5a
ilya0735 <iliaros0707@gmail.com>
2025-11-29 03:19:08 +0300

    Update user.py

--- Modules/user.py
+++ Modules/user.py
@@ -1,1 +1,11 @@
+from dataclasses import dataclass, field
+from typing import List
 
+# Определение класса User
+@dataclass(kw_only=True)
+class User:
+    id_user: int
+    user_name: str
+    user_viewed_films: List[str] = field(default_factory=list)
+    user_genre: List[str] = field(default_factory=list)
+    user_wish_list: List[str] = field(default_factory=list)
