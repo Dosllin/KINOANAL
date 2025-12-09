dc07f8c958d081e1a602cea2f1fefeba9914490f
ilya0735 <iliaros0707@gmail.com>
2025-12-10 02:39:36 +0300

    add country filter

--- Strategies/MultiStrategy.py
+++ Strategies/MultiStrategy.py
@@ -14,15 +14,15 @@ class MultiStrategy(StrategyRecommendation):
         films_list = []
         for strat in self.strats:
             films_list += strat
-            print(strat)
         counts = list(Counter(films_list).items())
         sorted_counts = sorted(counts, key=lambda x: x[1], reverse=True)
         return list(map(lambda x: x[0], sorted_counts))[:15]
 
     def filtered_year(self, min_year=0, max_year=9999):
-        sorted_list = self.strategy()
-        return list(filter(lambda x: min_year <= self.films_data[x]['year'] <= max_year, sorted_list))
+        return list(filter(lambda x: min_year <= self.films_data[x]['year'] <= max_year, self.strategy()))
 
     def filtered_rating(self, min_rating=0, max_rating=10):
-        sorted_list = self.strategy()
-        return list(filter(lambda x: min_rating <= sum(self.films_data[x]["rating"]) / len(self.films_data[x]["rating"]) <= max_rating, sorted_list))
+        return list(filter(lambda x: min_rating <= sum(self.films_data[x]["rating"]) / len(self.films_data[x]["rating"]) <= max_rating, self.strategy()))
+
+    def filtered_country(self, country):
+        return list(filter(lambda x: country in self.films_data[x]["countries"], self.strategy()))
