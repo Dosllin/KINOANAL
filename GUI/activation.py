19200e0c1d80970c05b08eaecf1b90fd6e3d08f4
Никита Прохоров <89354375+Dosllin@users.noreply.github.com>
2025-12-09 10:42:08 +0300

    + new filter

--- GUI/activation.py
+++ GUI/activation.py
@@ -4,12 +4,51 @@ from Strategies.DirectorStrategy import DirectorStrategy
 from Strategies.RatingStrategy import RatingStrategy
 from Strategies.Random import random_films
 from Strategies.MultiStrategy import MultiStrategy
+from Data.parsers import Parsers
+from thefuzz import process
+
 
 # Данные для фильтрации, если пользователь не ввёл фильтры
 filter_years = [-1000000000,100000000]
 filter_rating = -10
+country = ''
 
 
+def input_country_filter():
+    print('----------------------')
+    print("Введите название только одной страны")
+    print("Пример ввода: США")
+    print("Пример ввода: Россия")
+    films_data = Parsers.films_parser()
+    list_country = []
+    for name_film in films_data.keys():
+        list_country += films_data[name_film]['countries']
+    # print("Список стран:")
+    list_country = list(set(list_country))
+    country = input("Введите страну: ").strip()
+    results = process.extract(country, list_country, limit=5)
+    if country not in list_country:
+        print("Такой страны не найденно")
+        print(f"Возможно вы допустили ошибку в написании, вы имели в виду {results[0][0]}?")
+        print()
+        print('==============================')
+        print("1. Да (Сохранить выбранное значение)",
+              "2. Показать список стран",
+              "3. Нет (Выйти в главное меню)", sep='\n')
+        print('==============================')
+        print()
+        choice = input("Введите команду: ")
+        if choice == "1":
+            country = results[0][0]
+            return country
+        elif choice == "2":
+            for number, country in enumerate(list_country, start=1):
+                print(f"{number}. {country}")
+            return ""
+        else:
+            return ""
+    else:
+        return country
 def algorithm_menu():
     print("============================",
           "1. Начать работу алгоритма",
@@ -21,7 +60,8 @@ def filter_menu():
     print("==================================",
           "1. Установить года поиска",
           "2. Установить минимальный рейтинг",
-          "3. Выйти к работе алгоритма",
+          "3. Установить страну",
+          "4. Выйти к работе алгоритма",
           "==================================", sep="\n")
 
 
@@ -29,6 +69,7 @@ def filter_menu():
 def similar_algorithm(user, users_without_main_user):
     global filter_years
     global filter_rating
+    global country
     while 1:
         print('----------------------------')
         print('Алгоритм на основе пользователей')
@@ -41,8 +82,8 @@ def similar_algorithm(user, users_without_main_user):
 
             if user_choice == 1:
                 main_strategy = SimilarUsersStrategy(user, users_without_main_user)
-                films_list, films_list_little_similar = main_strategy.strategy(filter_years, filter_rating)[0], \
-                main_strategy.strategy(filter_years, filter_rating)[1]
+                films_list, films_list_little_similar = main_strategy.strategy(filter_years, filter_rating, country)[0], \
+                main_strategy.strategy(filter_years, filter_rating, country)[1]
                 show_a_recommended_movie(user, films_list) # У меня код в 2 частях повторялся, пай чарм посоветовал в отдельный деф закинуть
 
 
@@ -71,6 +112,8 @@ def similar_algorithm(user, users_without_main_user):
                         filter_rating = int(input("Введите минимальный рейтинг: "))
                         print('----------------------')
                     elif user_choice == 3:
+                        country = input_country_filter()
+                    elif user_choice == 4:
                         break
                     else:
                         print("Некорректный ввод")
@@ -122,6 +165,12 @@ def director_algorithm(user):
                         show_a_recommended_movie(user, main_strategy.filtered_rating(min_rating, max_rating))
                         print('----------------------')
                     elif user_choice == 3:
+                        print('----------------------')
+                        print("Введите название только одной страны")
+                        print("Пример ввода: США")
+                        print("Пример ввода: Россия")
+                        print('Введите страну: ')
+                    elif user_choice == 4:
                         break
                     else:
                         print("Некорректный ввод")
