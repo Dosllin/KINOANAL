0f4e67fde2bf661c9d47f3f95f6ca3892ddbf856
Sakaill <94001124+DedSKalashami@users.noreply.github.com>
2025-12-10 20:52:01 +0300

    фикс багов в стратегии фрагшиз

--- Strategies/FranchiseStrategy.py
+++ Strategies/FranchiseStrategy.py
@@ -10,7 +10,7 @@ class StrategyRecommendation(ABC):
         pass
 
 
-class StrategyGenre(StrategyRecommendation):
+class StrategyFranchise(StrategyRecommendation):
     def __init__(self, user):
         super().__init__(user)
         self.films_data = Parsers.films_parser()
@@ -21,28 +21,33 @@ class StrategyGenre(StrategyRecommendation):
         user_data = self.users[self.user.user_name]
         viewed_films = user_data["user_viewed_films"] # просмотренные фильмы пользователя
 
-        viewed_franchises = []
-
+        viewed_franchises = set()  # удаляем дубли
         for film in viewed_films:
-            viewed_franchises.append(self.films_data[film]["franchise"])
-
-        return viewed_franchises
+            # если фильма нет в базе — пропускаем
+            if film not in self.films_data:
+                continue
+            film_data = self.films_data[film]
+            # если нет ключа franchise или он пустой — пропускаем
+            if "franchise" not in film_data:
+                continue
+            if not film_data["franchise"]:
+                continue
+            viewed_franchises.add(film_data["franchise"])
+        return list(viewed_franchises)
 
 
     def strategy(self):
         user_franchises = self.get_user_franchises()
-        user_viewed = set(self.users[self.user.user_name]["user_viewed_films"])  # для удаления уже просмотренных
-
+        print("Франшизы пользователя:", user_franchises)
+        user_viewed = set(self.users[self.user.user_name]["user_viewed_films"])
         recommended_films = []
-
-        for franchise in user_franchises:
-
-            for film_name, film_data in self.films_data.items():
-                # не рекомендуем то, что пользователь уже видел
-                if film_name in user_viewed:
-                    continue
-                # если фильм есть во франшизе, добавляем
-                if franchise in film_data["franchises"]:
-                    recommended_films.append(film_name)
-
+        # проходим по всем фильмам в базе
+        for film_name, film_data in self.films_data.items():
+            film_franchise = film_data["franchise"]
+            # уже просмотренные - пропускаем или если у фильма нет поля franchise — пропускаем или franchise = ""
+            if film_name in user_viewed or "franchise" not in film_data or not film_franchise:
+                continue
+            # проверяем строгое совпадение франшизы
+            if film_franchise in user_franchises:
+                recommended_films.append(film_name)
         return recommended_films
