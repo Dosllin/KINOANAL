012217a6b81412be3f4a9fb89334ac8f2d28c68b
Sakaill <94001124+DedSKalashami@users.noreply.github.com>
2025-12-11 01:04:56 +0300

    сделаны фильтры для жанров и франшиз, мелкие исправления

--- GUI/activation.py
+++ GUI/activation.py
@@ -3,6 +3,7 @@ from Strategies.SimilarUsersStrategy import SimilarUsersStrategy
 from Strategies.DirectorStrategy import DirectorStrategy
 from Strategies.RatingStrategy import RatingStrategy
 from Strategies.StrategyGenre import StrategyGenre
+from Strategies.StrategyGenre import FranchiseStrategy
 from Strategies.Random import random_films
 from Strategies.MultiStrategy import MultiStrategy
 from Data.parsers import Parsers
@@ -267,7 +268,7 @@ def rating_algorithm(user):
 
 def genre_algorithm(user):
     while True:
-        print('----------------------')
+        print('----------------------------')
         print('Алгоритм на основе жанров')
         print('----------------------------')
 
@@ -275,16 +276,16 @@ def genre_algorithm(user):
 
         try:
             user_choice = int(input())
-
             if user_choice == 1:
                 main_strategy = StrategyGenre(user)
-                show_a_recommended_movie(user, main_strategy.strategy())
+                show_a_recommended_movie(user, main_strategy.strategy()[:10])
 
             elif user_choice == 2:
                 while 1:
                     print("==================================",
                           "1. Установить года поиска",
-                          "2. Установить страну поиска",
+                          "2. Установить страну",
+                          "3. Установить рейтинг"
                           "3. Выйти к работе алгоритма",
                           "==================================", sep="\n")
                     user_choice = int(input("Введите команду: "))
@@ -293,31 +294,26 @@ def genre_algorithm(user):
                         print('----------------------')
                         min_year = int(input("Введите минимальный год: "))
                         max_year = int(input("Введите максимальный год: "))
-                        main_strategy = StrategyGenre(user)
-                        show_a_recommended_movie(user, main_strategy.filtered_year(min_year, max_year))
+                        main_strategy = StrategyGenre(user).filtered_year(min_year, max_year)
+                        show_a_recommended_movie(user, main_strategy[:10])
+                        exist_checker(main_strategy)
                         print('----------------------')
 
                     elif user_choice == 2:
                         print('----------------------')
-                        country = input("Введите страну: ")
-                        all_countries = ['Австралия', 'Австрия', 'Алжир', 'Аргентина', 'Армения', 'Багамы', 'Бахрейн',
-                        'Беларусь','Бельгия', 'Болгария', 'Бразилия', 'Бруней-Даруссалам', 'Великобритания', 'Венгрия',
-                        'Вьетнам','Гамбия', 'Гваделупа', 'Гватемала', 'Германия', 'Германия (ГДР)', 'Германия (ФРГ)', 'Гонконг',
-                        'Греция', 'Грузия', 'Дания', 'Джибути', 'Доминикана', 'Египет', 'Замбия', 'Израиль', 'Индия',
-                        'Индонезия', 'Иордания', 'Ирак', 'Иран', 'Ирландия', 'Исландия', 'Испания', 'Италия', 'Йемен',
-                        'Казахстан', 'Каймановы острова', 'Камбоджа', 'Канада', 'Катар', 'Кения', 'Кипр', 'Китай',
-                        'Колумбия', 'Корея Северная', 'Корея Южная', 'Кыргызстан', 'Латвия', 'Ливан', 'Литва',
-                        'Лихтенштейн', 'Люксембург', 'Малайзия', 'Мальдивы', 'Мальта', 'Марокко', 'Мексика', 'Молдова',
-                        'Монако', 'Нидерланды', 'Новая Зеландия', 'Норвегия', 'ОАЭ', 'Перу', 'Польша', 'Португалия',
-                        'Россия', 'Румыния', 'СССР', 'США', 'Саудовская Аравия', 'Северная Македония', 'Сенегал',
-                        'Сент-Китс и Невис', 'Сербия', 'Сербия и Черногория', 'Сингапур', 'Словакия', 'Словения',
-                        'Таиланд', 'Тайвань', 'Тунис', 'Турция', 'Узбекистан', 'Украина', 'Уругвай', 'Филиппины',
-                        'Финляндия', 'Франция', 'Хорватия', 'Чехия', 'Чехословакия', 'Чили', 'Швейцария', 'Швеция',
-                        'Эквадор', 'Эстония', 'ЮАР', 'Югославия', 'Югославия (ФР)', 'Япония']
-                        if country not in all_countries:
-                            print("-------страна не найдена-------")
-                        main_strategy = StrategyGenre(user)
-                        show_a_recommended_movie(user, main_strategy.filtered_country(country))
+                        picked_country = input_country_filter()
+                        main_strategy = StrategyGenre(user.user_name).filtered_country(picked_country)
+                        show_a_recommended_movie(user, main_strategy[:10])
+                        exist_checker(main_strategy)
+                        print('----------------------')
+
+                    elif user_choice == 3:
+                        print('----------------------')
+                        min_rating = int(input("Введите минимальный рейтинг: "))
+                        max_rating = int(input("Введите максимальный рейтинг: "))
+                        main_strategy = StrategyGenre(user.user_name).filtered_rating(min_rating, max_rating)
+                        show_a_recommended_movie(user, main_strategy)
+                        exist_checker(main_strategy)
                         print('----------------------')
 
                     elif user_choice == 3:
@@ -339,6 +335,75 @@ def genre_algorithm(user):
             print("Нажмите Enter что бы продолжить")
             input()
 
+
+def franchise_algorithm(user):
+    while True:
+        print('----------------------------')
+        print('Алгоритм на основе жанров')
+        print('----------------------------')
+
+        algorithm_menu()
+
+        try:
+            user_choice = int(input())
+            if user_choice == 1:
+                main_strategy = FranchiseStrategy(user)
+                show_a_recommended_movie(user, main_strategy.strategy()[:10])
+
+            elif user_choice == 2:
+                while 1:
+                    print("==================================",
+                          "1. Установить года поиска",
+                          "2. Установить страну",
+                          "3. Установить рейтинг"
+                          "3. Выйти к работе алгоритма",
+                          "==================================", sep="\n")
+                    user_choice = int(input("Введите команду: "))
+
+                    if user_choice == 1:
+                        print('----------------------')
+                        min_year = int(input("Введите минимальный год: "))
+                        max_year = int(input("Введите максимальный год: "))
+                        main_strategy = FranchiseStrategy(user).filtered_year(min_year, max_year)
+                        show_a_recommended_movie(user, main_strategy[:10])
+                        exist_checker(main_strategy)
+                        print('----------------------')
+
+                    elif user_choice == 2:
+                        print('----------------------')
+                        picked_country = input_country_filter()
+                        main_strategy = FranchiseStrategy(user.user_name).filtered_country(picked_country)
+                        show_a_recommended_movie(user, main_strategy[:10])
+                        exist_checker(main_strategy)
+                        print('----------------------')
+
+                    elif user_choice == 3:
+                        print('----------------------')
+                        min_rating = int(input("Введите минимальный рейтинг: "))
+                        max_rating = int(input("Введите максимальный рейтинг: "))
+                        main_strategy = FranchiseStrategy(user.user_name).filtered_rating(min_rating, max_rating)
+                        show_a_recommended_movie(user, main_strategy)
+                        exist_checker(main_strategy)
+                        print('----------------------')
+
+                    elif user_choice == 3:
+                        break
+                    else:
+                        print("Некорректный ввод")
+                        print("Нажмите Enter что бы продолжить")
+                        input()
+
+            elif user_choice == 3:
+                break
+            else:
+                print("Некорректный ввод")
+                print("Нажмите Enter что бы продолжить")
+                input()
+
+        except ValueError:
+            print("Некорректный ввод")
+            print("Нажмите Enter что бы продолжить")
+            input()
 
 
 def multi_algorithm(user, users_without_main_user):
@@ -418,4 +483,3 @@ def multi_algorithm(user, users_without_main_user):
 def random_activation(user):
     list_random_films = random_films(user)
     show_a_recommended_movie(user, list_random_films)
-
